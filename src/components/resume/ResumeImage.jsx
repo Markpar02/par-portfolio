d383037f417b6834e70e8f6af6e07e0876e81381
Markpar02 <markangelo.par@hcdc.edu.ph>
2025-06-11 17:07:29 +0800

    UPDATED RESUMEIMAGE

--- src/components/resume/ResumeImage.jsx
+++ src/components/resume/ResumeImage.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
+import ParCV from '../assets/ParCV.pdf.jpg';
 
 const ResumeImage = () => {
   return (
     <section className="w-full py-20 flex justify-center">
       <img
-        src="/assets/ParCV.jpg"
+        src={ParCV}
         alt="Resume Preview"
         className="w-full max-w-4xl rounded-lg shadow-lg"
       />
